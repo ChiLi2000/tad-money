60ec71814c40b61db36eaaccd8df0458f34af83b
chili <1032569304@qq.com>
2020-09-26 15:53:16 +0800

    添加404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>当前网页不存在哦</div>
+    <div>
+      <router-link to="/">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
